8d13f127fc673ac406d659abf01b740e5b940c39
Jakob <jakicaa111@gmail.com>
2024-04-11 14:02:27 +0200

    Added a lot of test currently on testing CAN MSGs

--- Makefile
+++ Makefile
@@ -951,6 +951,7 @@ tst_test.moc: tst_test.cpp \
 		../qt-raspi/include/QtCore/QFile \
 		../qt-raspi/include/QtCore/QJsonDocument \
 		../qt-raspi/include/QtCore/QJsonObject \
+		../testBenchLibrary/leversactuator.h \
 		../qt-raspi/include/QtCore/qtmochelpers.h \
 		moc_predefs.h \
 		../qt-host/libexec/moc
@@ -1368,6 +1369,7 @@ tst_test.o: tst_test.cpp ../qt-raspi/include/QtTest/QtTest \
 		../qt-raspi/include/QtCore/QFile \
 		../qt-raspi/include/QtCore/QJsonDocument \
 		../qt-raspi/include/QtCore/QJsonObject \
+		../testBenchLibrary/leversactuator.h \
 		tst_test.moc \
 		../qt-raspi/include/QtCore/qtmochelpers.h
 	$(CXX) -c $(CXXFLAGS) $(INCPATH) -o tst_test.o tst_test.cpp
--- tst_test.cpp
+++ tst_test.cpp
@@ -5,6 +5,7 @@
 #include "../testBenchLibrary/keycardsactuator.h"
 #include "../testBenchLibrary/buttons.h"
 #include "../testBenchLibrary/camera.h"
+#include "../testBenchLibrary/leversactuator.h"
 #include <wiringPi.h>
 
 class Test : public QObject
@@ -21,17 +22,24 @@ private slots:
     void test_rightCardsDoesUnlock();
     void test_wrongCardDoesnNotLock();
     void test_rightCardLocks();
-    void test_pressDock();
+    void test_leverFWDAndCan();
+    void test_topLightsInNeutral();
+    void test_topLightsNotInNeutral();
+    void test_neutralSyncPressed();
+    void test_notNeutralSyncPressed();
+    void test_testCANMessages();
     void cleanupTestCase();
 
 private:
     const int m_POWER_PIN = 4;
     QCoreApplication *app{nullptr};
     QSerialPort *m_port{nullptr};
+    QSerialPort *m_port1{nullptr};
     KeyCardsActuator *m_KeyCardsActuator{nullptr};
     Buttons *m_buttons{nullptr};
     QCanBusDevice *m_device{nullptr};
     Camera *m_camera{nullptr};
+    LeversActuator *m_levers{nullptr};
 
 
     QList<QCanBusFrame> m_recivedFrames;
@@ -65,8 +73,19 @@ void Test::initTestCase()
         qDebug() << m_port->errorString();
     else qDebug()<< "Port open";
 
+    m_port1 = new QSerialPort("/dev/ttyACM2");
+    m_port1->setBaudRate(QSerialPort::Baud115200);
+    m_port1->setDataBits(QSerialPort::Data8);
+    m_port1->setStopBits(QSerialPort::OneStop);
+    m_port1->setParity(QSerialPort::NoParity);
+    m_port1->setFlowControl(QSerialPort::NoFlowControl);
+    if(!m_port1->open(QIODevice::ReadWrite))
+        qDebug() << m_port1->errorString();
+    else qDebug()<< "Port open";
+
     m_KeyCardsActuator = new KeyCardsActuator(m_port);
     m_camera = new Camera;
+    m_levers = new LeversActuator(m_port1);
     //m_port->clear();//digitalWrite(LED_PIN,HIGH);
 
     m_device = QCanBus::instance()->createDevice(
@@ -87,7 +106,7 @@ void Test::initTestCase()
 
 void Test::cleanupTestCase()
 {
-    //digitalWrite(m_POWER_PIN,HIGH);
+    digitalWrite(m_POWER_PIN,HIGH);
     m_port->close();
     delete m_KeyCardsActuator;
     delete m_port;
@@ -126,36 +145,37 @@ void Test::test_wrongCardsDoesNotUnlock()
 
 void Test::test_rightCardsDoesUnlock()
 {
-    //QSKIP("Test already tested!");
+    QSKIP("Test already tested!");
     //should be locked here
 
-    // QSignalSpy waitForRightCardMove(m_KeyCardsActuator, &KeyCardsActuator::rightCardFinishedMoving);
-    // m_KeyCardsActuator ->approchRightCard();
+    QSignalSpy waitForRightCardMove(m_KeyCardsActuator, &KeyCardsActuator::rightCardFinishedMoving);
+    m_KeyCardsActuator ->approchRightCard();
 
 
-    // auto wasRightMoveCalled = waitForRightCardMove.wait(7000);
-    // QCOMPARE(wasRightMoveCalled,true);
+    auto wasRightMoveCalled = waitForRightCardMove.wait(7000);
+    QCOMPARE(wasRightMoveCalled,true);
 
-    // bool wasLeverUnlocked = false;
+    bool wasLeverUnlocked = false;
 
-    // foreach(auto frame, m_recivedFrames){
-    //     if((frame.frameId() == 0x18ff82fd) && frame.payload()[2] == 0x01)
-    //     {
-    //         wasLeverUnlocked = true;
-    //     }
-    // }
+    foreach(auto frame, m_recivedFrames){
+        if((frame.frameId() == 0x18ff82fd) && frame.payload()[2] == 0x01)
+        {
+            wasLeverUnlocked = true;
+        }
+    }
 
-    // QSignalSpy waitForPicture(m_camera,&Camera::pictureTaken);
-    // m_camera->takePicture();
+    QCOMPARE(wasLeverUnlocked,true);
 
-    // auto wasPictureTaken=waitForPicture.wait(10000);
+    QSignalSpy waitForPicture(m_camera,&Camera::pictureTaken);
+    m_camera->takePicture();
 
-    //QCOMPARE(wasPictureTaken,true);
-    m_camera->processData();
-    //delay(10000);
+    auto wasPictureTaken=waitForPicture.wait(10000);
 
-    //QCOMPARE(wasLeverUnlocked,true);
-    QCOMPARE(true,true);
+    QCOMPARE(wasPictureTaken,true);
+
+    qDebug()<<"Sync Button status: "<<m_camera->getStatus(syncBUTTON);
+
+    QCOMPARE(m_camera->getStatus(syncBUTTON),true);
 
 
 }
@@ -219,16 +239,179 @@ void Test::test_rightCardLocks()
 
 }
 
-void Test::test_pressDock()
+void Test::test_topLightsInNeutral()
 {
     QSKIP("");
-    QSignalSpy spy(m_buttons, &Buttons::pressed);
-    m_buttons->press(sync);
-    //m_buttons->press(station); not really work
-    m_buttons->press(dock);
-    auto wasSpyCalled=spy.wait(6000);
+    QSignalSpy waitForLeverUnlock(m_KeyCardsActuator, &KeyCardsActuator::rightCardFinishedMoving);
+    m_KeyCardsActuator-> approchRightCard();
+    auto isLeverUnlocked=waitForLeverUnlock.wait(7000); //unlocking card
+    QCOMPARE(isLeverUnlocked,true);
+    // delay(3000);
+    //m_levers->home();
+    QSignalSpy waitForReverseMove(m_levers,&LeversActuator::bothMoved);
+    m_levers->moveBoth(LeversActuator::reverse,700);
+    auto didBothMove=waitForReverseMove.wait(7000);
+    QCOMPARE(didBothMove,true);
 
-    QCOMPARE(wasSpyCalled,true);
+    QSignalSpy waitForPictureTaken(m_camera, &Camera::pictureTaken);
+    m_camera->takePicture();
+    auto didWeTakePicture=waitForPictureTaken.wait(5000);
+    //take picture
+    QCOMPARE(didWeTakePicture,true);
+    // delay(5000);
+    QSignalSpy waitForLeversHome(m_levers, &LeversActuator::bothHome);
+    m_levers->home();
+    auto isLeversHome=waitForLeversHome.wait(7000);
+    //lever not sending messages
+
+    QCOMPARE(isLeversHome,true);
+    // QSignalSpy spy(m_buttons, &Buttons::pressed);
+    // m_buttons->press(sync);
+    //m_buttons->press(station); not really work
+    // m_buttons->press(dock);
+    // auto wasSpyCalled=spy.wait(6000);
+
+    //QCOMPARE(true,true);
+    QCOMPARE(m_camera->getStatus(leftLEVER),true);
+    QCOMPARE(m_camera->getStatus(rightLEVER),true);
+
+
+}
+
+void Test::test_topLightsNotInNeutral()
+{
+    QSKIP("");
+    //unlock lever
+    QSignalSpy waitForLeverUnlock(m_KeyCardsActuator, &KeyCardsActuator::rightCardFinishedMoving);
+    m_KeyCardsActuator->approchRightCard();
+    auto isLeverUnlocked=waitForLeverUnlock.wait(7000);
+    QCOMPARE(isLeverUnlocked,true);
+
+    QSignalSpy waitForPictureTaken(m_camera, &Camera::pictureTaken);
+    m_camera->takePicture();
+    auto wasPictureTaken=waitForPictureTaken.wait(5000);
+    QCOMPARE(wasPictureTaken,true);
+
+    QCOMPARE(m_camera->getStatus(leftLEVER),false);
+    QCOMPARE(m_camera->getStatus(rightLEVER),false);
+
+}
+
+void Test::test_neutralSyncPressed()
+{
+    //First unlock lever
+    QSKIP("");
+    QSignalSpy waitForLeverUnlock(m_KeyCardsActuator, &KeyCardsActuator::rightCardFinishedMoving);
+    m_KeyCardsActuator->approchRightCard();
+    auto isLeverUnlocked=waitForLeverUnlock.wait(7000);
+    QCOMPARE(isLeverUnlocked,true);
+
+    QSignalSpy waitForReverseMove(m_levers,&LeversActuator::bothMoved);
+    m_levers->moveBoth(LeversActuator::reverse,700);
+    auto didBothMove=waitForReverseMove.wait(7000);
+    QCOMPARE(didBothMove,true);
+
+    QSignalSpy waitForPressedSync(m_buttons,&Buttons::pressed);
+    m_buttons->press(sync);
+    auto isSyncPressed=waitForPressedSync.wait(5000);
+    QCOMPARE(isSyncPressed,true);
+
+    bool wasSyncActivated=false;
+
+    foreach(auto frame, m_recivedFrames){
+        if((frame.frameId() == 0x18FF82FD) && frame.payload()[1] == 0x03)
+        {
+            wasSyncActivated = true;
+        }
+
+    }
+
+
+
+    QSignalSpy waitForLeversHome(m_levers, &LeversActuator::bothHome);
+    m_levers->home();
+
+    auto isLeversHome=waitForLeversHome.wait(7000);
+    QCOMPARE(isLeversHome,true);
+
+
+    QCOMPARE(wasSyncActivated,true); //Not getting sync, prob lever software fault (single mode prob)
+
+
+}
+
+void Test::test_notNeutralSyncPressed()
+{
+    //Lever should be unlocked
+    QSKIP("");
+    QSignalSpy waitForLeverUnlock(m_KeyCardsActuator, &KeyCardsActuator::rightCardFinishedMoving);
+    m_KeyCardsActuator->approchRightCard();
+    auto isLeverUnlocked=waitForLeverUnlock.wait(5000);
+    QCOMPARE(isLeverUnlocked,true);
+
+    QSignalSpy waitForPressedSync(m_buttons, &Buttons::pressed);
+    m_buttons->press(sync);
+    auto isSyncPressed=waitForPressedSync.wait(6000);
+    QCOMPARE(isSyncPressed,true);
+
+
+    bool wasSyncActivated=false;
+
+    foreach(auto frame, m_recivedFrames){
+        if((frame.frameId() == 0x18FF82FD) && frame.payload()[1] == 0x03)
+        {
+            wasSyncActivated = true;
+        }
+
+    }
+
+    QCOMPARE(wasSyncActivated,false);
+
+
+
+    foreach(auto frame, m_recivedFrames){
+        qDebug()<<frame.toString();
+
+    }
+
+
+    QCOMPARE(true,true);
+
+
+
+
+}
+
+void Test::test_testCANMessages()
+{
+    // QSKIP("");
+    //unlock lever first
+
+    QSignalSpy waitForUnlocked(m_KeyCardsActuator,&KeyCardsActuator::rightCardFinishedMoving);
+    m_KeyCardsActuator->approchRightCard();
+
+    auto wasLeverUnlocked=waitForUnlocked.wait(7000);
+    QCOMPARE(wasLeverUnlocked,true);
+
+
+    QSignalSpy delayTenSeconds(m_KeyCardsActuator,&KeyCardsActuator::ghost);
+
+    QCOMPARE(delayTenSeconds.wait(10000),false);
+
+
+
+
+
+
+
+}
+
+void Test::test_leverFWDAndCan()
+{
+    QSKIP("");
+    //should write everything but CAN read
+    // cant do it yet since lever is not sending data!
+
 }
 
 
